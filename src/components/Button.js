49bee20df08240c3cb793a488ab1c30aea598eab
Niharika <niharika.202310@gmail.com>
2024-06-30 16:20:30 +0530

    added favicon

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,9 +3,13 @@ import './Button.css'
 
 export default function Button(props) {
   const btn=props.primary?'primarybtn': 'secondarybtn';
+  const click=()=>{
+    let txt=props.text;
+    console.log("you clicked "+txt+" button");
+  }
   return (
     <div>
-      <button className={btn}>
+      <button className={btn} onClick={click}>
         <div>
         {props.icon}
         </div>
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ContactForm.css'
 import Button from './Button'
 import { MdMessage } from 'react-icons/md'
 import { FaPhoneAlt } from 'react-icons/fa'
 import { HiMail } from 'react-icons/hi'
+import logo from '../images/img.svg'
 
 export default function ContactForm() {
+  const [name,setName]=useState("xyz");
+  const onsubmit=(e)=>{
+    e.preventDefault();
+    console.log(e);
+    setName(e.target[0].value);
+    console.log("name",e.target[0].value);
+    console.log("email",e.target[1].value);
+    console.log("text",e.target[2].value);
+  }
   return (
     <>
     <div className='contactform'>
@@ -17,10 +27,29 @@ export default function ContactForm() {
             </div>
             <div className='contactbtn'>
               <Button text="VIA EMAIL FORM" primary={false} icon={<HiMail fontSize="24px"/>} />
-
             </div>
+            <form onSubmit={onsubmit}>
+              <div className='formctrl'>
+                <label htmlFor="name"  >Name</label>
+                <input type="text" name='name' />
+              </div>
+              <div className='formctrl formemail'>
+                <label htmlFor="email">E-Mail</label>
+                <input type="email" name='email' />
+              </div>
+              <div className='formctrl'>
+                <label htmlFor="text">Text</label>
+                <textarea name="text" id="" rows={4} ></textarea>
+              </div>
+              <div className='submitbtn'>
+                <Button primary={true} text="SUBMIT"/>
+              </div>
+              {/* <p>{name + " follows coding"}</p> */}
+            </form>
         </div>
-        <div className='contactimg'></div>
+        <div className='contactimg'>
+          <img src={logo} alt="" />
+        </div>
     </div> 
     </>
   )
